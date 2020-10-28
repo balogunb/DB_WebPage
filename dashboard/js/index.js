524e3c02537482c77b1a71aeb07224eb227a5bf8
?????? <modemfail123@gmail.com>
2020-10-27 22:37:03 -0400

    Add some vue stuff that doesn't work yet

--- dashboard/js/index.js
+++ dashboard/js/index.js
@@ -1,1 +1,15 @@
+import {Line, mixins } from 'vue-chartjs'
+const { reactiveProp } = mixins
 
+const app = Vue.createApp()
+
+
+const ourChart = {
+
+	extends: Line,
+	mixins: [reactiveProp],
+	props: ['options'], //chartData prop is made automatically, and we can modify it
+	mounted () {
+		this.renderChart(this.chartdata, this.options)
+	}
+}
