a808a44290d8ea78f01b2922b06ca73617e22f59
dorwardc <dorwardc@lafayette.edu>
2020-10-28 23:36:17 -0400

    Pretty Colors (kindof)

--- dashboard-vue2/src/components/RandomChart.vue
+++ dashboard-vue2/src/components/RandomChart.vue
@@ -37,68 +37,68 @@
         datasets: [
           {
             label: 'Carbon County Cases',
-            backgroundColor: '#f87979',
+            backgroundColor: '#ccf5ac',
             data: [10, 31, 34, 29, 14]
             }, {
               label: 'Carbon County Deaths',
-              backgroundColor: '#f89779',
+              backgroundColor: '#bad29f',
               data: [0, 1, 0, 0, 0]
             }, {
               label: 'Lehigh County Cases',
-              backgroundColor: '#f87979',
+              backgroundColor: '#808a9f',
               data: [134, 210, 168, 267, 165]
               }, {
               label: 'Lehigh County Deaths',
-              backgroundColor: '#f89779',
+              backgroundColor: '#2c4975',
               data: [1, 3, 4, 4, 0]
               }, {
               label: 'Northampton County Cases',
-              backgroundColor: '#f87979',
+              backgroundColor: '#3d2b56',
               data: [78, 266, 202, 247, 72]
               }, {
               label: 'Northampton County Deaths',
-              backgroundColor: '#f89779',
+              backgroundColor: '#231123',
               data: [0, 1, 3, 2, 0]
               }, {
               label: 'Warren County Cases',
-              backgroundColor: '#f87979',
+              backgroundColor: '#82204a',
               data: [1, 8, 7, 3, 2]
               }, {
               label: 'Warren County Deaths',
-              backgroundColor: '#f89779',
+              backgroundColor: '#b20d30',
               data: [0, 0, 0, 0, 0]
               }, {
               label: 'Cedar Crest College Cases',
-              backgroundColor: '#f87979',
+              backgroundColor: '#bfae48',
               data: [1, 1, 0, 0, 1]
               }, {
               label: 'DeSales University Cases',
-              backgroundColor: '#f87979',
+              backgroundColor: '#5fad41',
               data: [1, 1, 0, 2, 2]
             },
             {
               label: 'Muhlenberg College Cases',
-              backgroundColor: '#f87979',
+              backgroundColor: '#262730',
               data: [1, 0, 0, 0, 1]
               },{
                 label: 'Pennsylvania State University Lehigh Valley Cases',
-                backgroundColor: '#f87979',
+                backgroundColor: '#2d936c',
                 data: [0, 0, 0, 1, 0]
                 },{
                   label: 'Lafayette College Cases',
-                  backgroundColor: '#f87979',
+                  backgroundColor: '#391463',
                   data: [0, 0, 10, 8, 1]
                   },{
                     label: 'Cedar Crest College Cases',
-                    backgroundColor: '#f87979',
+                    backgroundColor: '#3a0842',
                     data: [89, 85, 38, 10, 11]
                     },{
                       label: 'Lehigh University Cases',
-                      backgroundColor: '#f87979',
+                      backgroundColor: '#d33f49',
                       data: [89, 85, 38, 10, 11]
                       },{
                         label: 'Moravian College Cases',
-                        backgroundColor: '#f87979',
+                        backgroundColor: '#d7c0d0',
                         data: [1, 0, 0, 0, 1]
                         }
             ]
