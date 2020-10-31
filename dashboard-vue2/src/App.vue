0f515181b48a80b1af6cf2a7a24a57ad204205f3
?????? <modemfail123@gmail.com>
2020-10-31 16:03:41 -0400

    Add custom dataset support

--- dashboard-vue2/src/App.vue
+++ dashboard-vue2/src/App.vue
@@ -2,7 +2,7 @@
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
     <HelloWorld msg="Welcome to Your Vue.js App"/>
-    <RandomChart :stack="false"></RandomChart>
+    <RandomChart :customdata="chart1data" :stack="false"></RandomChart>
   </div>
 </template>
 
@@ -15,6 +15,27 @@ export default {
   components: {
     // HelloWorld,
     RandomChart
+  },
+  data () {
+    return {
+      chart1data: {
+        labels: ['09/27/2020', '10/04/2020', '10/11/2020', '10/18/2020', '10/25/2020'],
+        datasets: [
+          {
+            label: 'Carbon County Cases',
+            backgroundColor: '#f87979',
+            data: [10, 31, 34, 29, 14],
+            fill: false
+            }, 
+          {
+            label: 'Carbon County Deaths',
+            backgroundColor: '#f89779',
+            data: [0, 1, 0, 0, 0],
+            fill: false
+            }
+          ]
+        }
+    }
   }
 }
 </script>
--- dashboard-vue2/src/components/RandomChart.vue
+++ dashboard-vue2/src/components/RandomChart.vue
@@ -12,10 +12,10 @@
     components: {
       LineChart
     },
-    props: ['stack'],
+    props: ['stack', 'customdata'],
     data () {
       return {
-        datacollection: null,
+        datacollection: this.customdata,
         opts: {
           scales: {
             yAxes: [{
@@ -27,27 +27,8 @@
     },
     mounted () {
       this.opts.scales.yAxes[0].stacked = this.stack
-      this.fillData()
-
     },
     methods: {
-      fillData () {
-        this.datacollection = {
-        labels: ['09/27/2020', '10/04/2020', '10/11/2020', '10/18/2020', '10/25/2020'],
-        datasets: [
-          {
-            label: 'Carbon County Cases',
-            backgroundColor: '#f87979',
-            data: [10, 31, 34, 29, 14]
-            }, 
-          {
-            label: 'Carbon County Deaths',
-            backgroundColor: '#f89779',
-            data: [0, 1, 0, 0, 0]
-            }
-          ]
-        }
-      },
       getRandomInt () {
         return Math.floor(Math.random() * (50 - 5 + 1)) + 5
       }
