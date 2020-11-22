6a6ee572a0b8e1bff94ec2e7030486825e449713
?????? <modemfail123@gmail.com>
2020-11-21 21:26:28 -0500

    Add functionality to all charts

--- dashboard-vue2/src/App.vue
+++ dashboard-vue2/src/App.vue
@@ -4,10 +4,12 @@
 		<HelloWorld msg="Welcome to Your Vue.js App"/>-->
 		<h1>DreamTeam Lehigh Valley COVID-19 Dashboard</h1>
 		<h2>Cases and Deaths (County)</h2>
-		<v-select label="name" :options="counties" v-model="selected" :reduce="value => value.name"> </v-select>
+		<v-select label="name" :clearable="false" :options="counties" v-model="selected" :reduce="value => value.name"> </v-select>
 		<RandomChart :customdata="chart1data" :stack="false"></RandomChart>
-		<h2>Cases and Deaths (All Counties)</h2>
-		<RandomChart :customdata="chart2data" :stack="true"></RandomChart>
+		<h2>Cases (All Counties)</h2>
+		<RandomChart :customdata="chart2data" :displayAverage="true" :displayFloats="true" :stack="true" 	></RandomChart>
+		<h2>Cases by College in County </h2>
+		<RandomChart :customdata = "chart3data" :stack="false"></RandomChart>
 		<h2>Cases at All Colleges and Total Cases</h2>
 		<RandomChart :customdata="chart4data" :stack="false"></RandomChart>
 
@@ -26,118 +28,149 @@ export default {
 		return {
 			selected: {}, //this is the object selected by the county drop-down.
 			counties: [], //the list of objects selectable by the county drop-down.
+			countyPops: {},
 			chart1data: {},
-			chart2data: {
-				labels: ['09/27/2020', '10/04/2020', '10/11/2020', '10/18/2020', '10/25/2020'],
-				datasets: [
-					{
-							label: 'Warren County Cases',
-							backgroundColor: '#f87979',
-							data: [1, 8, 7, 3, 2]
-						},
-					{
-							label: 'Northampton County Cases',
-							backgroundColor: '#f21979',
-							data: [78, 266, 202, 247, 72]
-						},
-					{
-							label: 'Lehigh County Cases',
-							backgroundColor: '#f8b979',
-							data: [134, 210, 168, 267, 165]
-						},
-					{
-							label: 'Carbon County Cases',
-							backgroundColor: '#f87000',
-							data: [10, 31, 34, 29, 14]
-						}
-				]
-			},
-			chart4data: {
-					labels: ['09/27/2020', '10/04/2020', '10/11/2020', '10/18/2020', '10/25/2020'],
-					datasets: [
-						{
-							label: 'Total County Cases',
-							borderColor: '#f87979',
-							data: [223, 515, 411, 546, 253],
-							fill: false
-							}, 
-						{
-							label: 'Cedar Crest College Cases',
-							borderColor: '#f87979',
-							data: [1, 1, 0, 0, 1],
-							fill: false
-							},
-						{
-							label: 'DeSales University Cases',
-							borderColor: '#f87979',
-							data: [1, 1, 0, 2, 2],
-							fill: false
-							},
-						{
-							label: 'Muhlenberg College Cases',
-							borderColor: '#f87979',
-							data: [1, 0, 0, 0, 1],
-							fill: false
-							},
-						{
-							label: 'Pennsylvania State University Lehigh Valley Cases',
-							borderColor: '#f87979',
-							data: [0, 0, 0, 1, 0],
-							fill: false
-							},
-						{
-							label: 'Lafayette College Cases',
-							borderColor: '#f87979',
-							data: [0, 0, 10, 8, 1],
-							fill: false
-							},
-						{
-							label: 'Lehigh University Cases',
-							borderColor: '#f87979',
-							data: [89, 85, 38, 10, 11],
-							fill: false
-							},
-						{
-							label: 'Moravian College Cases',
-							borderColor: '#f87979',
-							data: [1, 0, 0, 0, 1],
-							fill: false
-							}
-						]
-				}
+			chart2data: {},
+			chart3data: {},
+			chart4data: {}
 		}
 	},
 	watch: {
 		selected : function (newSelected) {
 			this.$http.get('http://139.147.9.191:80/countydata', {params: {county_name : newSelected}}).then(response => {
-				let data = {
-					labels: response.data.map( week => week.date.match(/[^T]*/i)[0]),
-					datasets: [
-						{
+				let timestamps = response.data.map( week => week.date.match(/[^T]*/i)[0])
+				let countyDeaths = {
 							label: newSelected + " Deaths",
 							data: response.data.map( week => week.new_deaths),
 							backgroundColor: '#f87979',
 							borderColor: '#f87979',
 							fill: false
-							},
-						{
+				}
+				let countyCases = {
 							label: newSelected + " Cases",
 							data: response.data.map( week => week.new_cases),
 							backgroundColor: '#f21979',
 							borderColor: '#f21979',
 							fill: false
-							}	
-					]
-			}
-			this.chart1data = data
+				}
+				let c1data = {
+					labels: timestamps,
+					datasets: [countyCases, countyDeaths]
+				}
+				this.chart1data = c1data
+
+				this.$http.get('http://139.147.9.191:80/countycolleges', {params: {county_name: newSelected}}).then(response_colleges => {
+					let collegeRequests = response_colleges.data.map(college => this.$http.get('http://139.147.9.191:80/collegedata', {
+						params: {
+							college_name: college.name}
+						}
+					))
+
+					this.$http.all(collegeRequests).then( colleges => {
+						// initially, countyCollegeDatasets contains a dataset for every college in the county. This can be an empty array at this point!
+						let countyCollegeDatasets = colleges.map(college => {
+							//we need to pad out the left side of our dataset because college records don't extend as far back as county records
+							let alignedData = new Array(timestamps.length - college.data.length).fill(0).concat(college.data.map(week => week.new_cases))
+							return {
+								label: college.data[0].college_name,
+								data: alignedData,
+								backgroundColor: '#f87897',
+								borderColor: '#f87897',
+								fill: false
+							}
+						})
+						//add the overall county data to countyCollegeDatasets
+						countyCollegeDatasets.push(countyCases)
+						this.chart3data = {
+							labels: timestamps,
+							datasets: countyCollegeDatasets
+						}
+					})	 
+				})
 			})
 		}
 	},
 	mounted () {
 		this.$http.get('http://139.147.9.191:80/counties').then(response => {
 			this.counties = response.data
-			this.selected = response.data[0].name //watcher loads data for this default value
+			this.countyPops = Object.assign({}, ...response.data.map(county => ({[county.name]: county.population})))
+			this.selected = response.data[0].name //watchers load county-specific data into charts 1 & 3 for this default value
+
+
+			let countyRequests = response.data.map(county => this.$http.get('http://139.147.9.191:80/countydata', {params: {county_name: county.name}}))
+			this.$http.all(countyRequests).then(counties => {
+				let timestamps = counties[0].data.map( week => week.date.match(/[^T]*/i)[0])
+				let countyIncidenceDatasets = counties.map(county => {
+					return {
+						label: county.data[0].county_name,
+						data: county.data.map( week => {
+							console.log(week.new_cases / this.countyPops[week.county_name])
+							return (1000 * week.new_cases / this.countyPops[week.county_name])
+						}),
+						backgroundColor: '#f27997',
+						borderColor: '#f27997',
+						fill: false
+					}
+
+					
+				})
+				console.log(countyIncidenceDatasets)
+
+				this.chart2data = {
+					labels: timestamps,
+					datasets: countyIncidenceDatasets
+				}
+
+				let valleyTotalCases = []
+				let total = 0
+				for (let i = 0; i < counties[0].data.length; i++){
+					for (const county of counties){
+						total += county.data[i].new_cases
+					}
+					valleyTotalCases.push(total)
+				}
+				let valleyTotalsDataset = {
+					label: "Lehigh Valley Cases",
+					data: valleyTotalCases,
+					backgroundColor: '#f27997',
+					borderColor: '#f27997',
+					fill: false
+				}
+
+				this.$http.get('http://139.147.9.191:80/colleges').then(response_colleges => {
+
+					let collegeRequests = response_colleges.data.map(college => this.$http.get('http://139.147.9.191:80/collegedata', {params: {college_name: college.name}}))
+				
+					this.$http.all(collegeRequests).then( colleges => {
+						//valleyCollegeDatasets contains a dataset for every college in the Lehigh Valley.
+						let valleyCollegeDatasets = colleges.map(college => {
+							//we need to pad out the left side of our dataset because college records don't extend as far back as county records
+							let alignedData = new Array(timestamps.length - college.data.length).fill(0).concat(college.data.map(week => week.new_cases))
+							
+							return {
+								label: college.data[0].college_name,
+								data: alignedData,
+								backgroundColor: '#f87897',
+								borderColor: '#f87897',
+								fill: false
+							}
+						})
+						//add the overall county data to countyCollegeDatasets
+						valleyCollegeDatasets.push(valleyTotalsDataset)
+						
+						this.chart4data = {
+							labels: timestamps,
+							datasets: valleyCollegeDatasets
+						}
+					})
+				})
+
+			})
 		})
+
+
+
+
 	}
 }
 </script>
--- dashboard-vue2/src/components/RandomChart.vue
+++ dashboard-vue2/src/components/RandomChart.vue
@@ -1,46 +1,76 @@
 <template>
-  <div class="small">
-    <line-chart :chart-data="datacollection" :options="opts"></line-chart>
-  </div>
+	<div class="small">
+		<line-chart :chart-data="datacollection" :options="opts"></line-chart>
+	</div>
 </template>
 
 <script>
 //  import Vue from 'vue'
-  import LineChart from './LineChart.js'
+	import LineChart from './LineChart.js'
 
-  export default {
-    components: {
-      LineChart
-    },
-    props: ['stack', 'customdata'],
-    data () {
-      return {
-        datacollection: this.customdata,
-        opts: {
-          scales: {
-            yAxes: [{
-              stacked: this.stack
-            }]
-          }
-        }
-      }
-    },
-    methods: {
-      getRandomInt () {
-        return Math.floor(Math.random() * (50 - 5 + 1)) + 5
-      }
-    },
-    watch: {
-      customdata: function (newData) {
-        this.datacollection = newData      
-      }
-    }
-  }
+	export default {
+		components: {
+			LineChart
+		},
+		props: ['stack', 'customdata', 'displayAverage', 'displayFloats'],
+		data () {
+			return {
+				datacollection: this.customdata,
+				opts: {
+					tooltips: {
+						mode: 'index',
+						intersect: false,
+						callbacks: {}
+					},
+					hover: {
+						mode: 'index',
+						intersect: false
+					},
+					scales: {
+						yAxes: [{
+							stacked: this.stack
+						}]
+					}
+				}
+			}
+		},
+		methods: {
+			getRandomInt () {
+				return Math.floor(Math.random() * (50 - 5 + 1)) + 5
+			}
+		},
+		watch: {
+			customdata: function (newData) {
+				this.datacollection = newData      
+			}
+		},
+		created () {
+			if (this.displayAverage){
+				this.opts.tooltips.callbacks.footer = function(tooltipItems) {
+					let total = 0
+					for (let i = 0; i < tooltipItems.length; i++){
+						total += parseFloat(tooltipItems[i].value)
+					}
+					return "Lehigh Valley Total: " + (total).toFixed(3) + " per 4000 (" + (total / 4).toFixed(3) + " per 1000)"
+				}
+			}
+			if (this.displayFloats){
+				this.opts.tooltips.callbacks.label = function (tooltipItem, data) {
+				console.log(tooltipItem)
+				console.log(data)
+				return data.datasets[tooltipItem.datasetIndex].label + ": " + data.datasets[tooltipItem.datasetIndex].data[tooltipItem.index].toFixed(3) + " per 1000"
+
+				}
+			}
+		}
+
+
+	}
 </script>
 
 <style>
-  .small {
-    max-width: 600px;
-    margin:  150px auto;
-  }
+	.small {
+		max-width: 600px;
+		margin:  150px auto;
+	}
 </style>
